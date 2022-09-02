161cad65826326b52efc9e1ff3d695e08a02f10e
Lawrence <lawrencemurry@yahoo.com>
2022-09-02 06:05:59 +0000

    Updated code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,11 +52,11 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  # config.web_socket_server_url = "wss://action-cable-abnbclone.herokuapp.com/cable"
-  # config.action_cable.allowed_request_origins = ['https://action-cable-abnbclone.herokuapp.com/cable', 'https://action-cable-abnbclone.herokuapp.com/cable' ]
+  config.web_socket_server_url = "wss://action-cable-abnbclone.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://action-cable-abnbclone.herokuapp.com/cable', 'https://action-cable-abnbclone.herokuapp.com/cable' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
