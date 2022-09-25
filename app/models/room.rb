93d4b8704de1b14068c81eaf1a4537c5d7f4891e
Lawrence <lawrencemurry@yahoo.com>
2022-09-25 20:57:44 +0000

    Updated code

--- app/models/room.rb
+++ app/models/room.rb
@@ -10,12 +10,6 @@ class Room < ApplicationRecord
     
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
-  
-  validates :home_type, presence: true
-  validates :room_type, presence: true
-  validates :accommodate, presence: true
-  validates :bed_room, presence: true
-  validates :bath_room, presence: true
 
   def cover_photo(size)
     if self.photos.length > 0
